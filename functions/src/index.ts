62bcac9f4352e3f19a3b730fa97945b553a21c14
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-26 12:52:12 +0100

    Add functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-const functions = require('firebase-functions');
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
