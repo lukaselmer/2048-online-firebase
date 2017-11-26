e14c640662925bc9c2329823147f94f1084f186b
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-26 13:26:30 +0100

    Scaffold

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,26 @@
+import { Request, Response } from 'express';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send('Hello from Firebase!');
-// });
+exports.turnStart = functions.https.onRequest((request, response) => {
+  const user = getUser(request);
+  console.log(request);
+  response.send('turn started');
+});
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
- });
+exports.move = functions.https.onRequest((request, response) => {
+  console.log(request);
+  response.send('moved');
+});
+
+exports.gameOver = functions.https.onRequest((request, response) => {
+  console.log(request);
+  response.send('game over');
+});
+
+function getUser(request: Request) {
+  // TODO
+}
