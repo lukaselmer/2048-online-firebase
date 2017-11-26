de7994ff17bcb933e154284d3cbebc09a8eb4a00
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-26 16:32:35 +0100

    Implement queue

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,25 +2,22 @@ import { Request, Response } from 'express';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-type User = admin.auth.DecodedIdToken;
+class User {
+  uid: string;
+  active: boolean;
+  name: string;
+  score: number;
+}
 
 admin.initializeApp((functions.config() as any).firebase);
 
 const db = admin.database();
+const store = admin.firestore();
 const game = db.ref('game');
 const queue = db.ref('queue');
-const users = db.ref('users');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-// tslint:disable-next-line:no-var-requires
-// const express = require('express');
-// tslint:disable-next-line:no-var-requires
 
 exports.turnStart = functions.https.onRequest(async (request, response) => {
   const user = await getUser(request);
-  console.log(user);
 
   removePreviousPlayersFromQueue(user);
   setTopOfQueuePlaying(user);
@@ -28,8 +25,8 @@ exports.turnStart = functions.https.onRequest(async (request, response) => {
   response.send('turn started');
 });
 
-exports.move = functions.https.onRequest((request, response) => {
-  const user = getUser(request);
+exports.move = functions.https.onRequest(async (request, response) => {
+  const user = await getUser(request);
   setGameState(request);
   updateScore(user, request);
   fillQueue();
@@ -37,32 +34,45 @@ exports.move = functions.https.onRequest((request, response) => {
   response.send('moved');
 });
 
-exports.refreshQueue = functions.https.onRequest((request, response) => {
+exports.refreshQueue = functions.https.onRequest(async (request, response) => {
   fillQueue();
-
   response.send('filled');
 });
 
-function getUser(request: Request): Promise<any> {
-  console.log('User ID:');
-  console.log(request.query);
-  console.log(request.body);
+async function getUser(request: Request): Promise<User> {
+  // console.log(request.query);
+  // console.log(request.body);
+  const path = 'users/' + request.query.userId;
 
-  return db.ref('users/' + request.query.userId).once('value');
+  const snapshot = await store
+    .collection('users')
+    .doc(request.query.userId)
+    .get();
+  return snapshot.data() as any;
 }
 
-function fillQueue() {
-  queue.once('value').then(snapshot => {
-    snapshot.val();
-  });
+async function fillQueue() {
+  const snapshot = await queue.once('value');
+  const newQueue = snapshot.val() || [];
+  const userSnapshots = await store.collection('users').get();
+  const users: User[] = [];
+  userSnapshots.forEach(sn => users.push(sn.data() as any));
+  if (newQueue.length < 5) {
+    newQueue.push(users[Math.floor(Math.random() * users.length)]);
+  }
+  queue.set(newQueue);
 }
 
 function setGameState(request: Request) {
   const gameState = request.body;
-  game.set(gameState);
+  game.set(gameState.gameState);
 }
 
-function updateScore(user: any, request: Request) {}
+function updateScore(user: User, request: Request) {
+  const gameState = request.body;
+  const additionalScore = gameState.additionalScore;
+  db.ref(`users/${user.uid}/score`).set(user.score + additionalScore);
+}
 
 function removePreviousPlayersFromQueue(user: User) {}
 
