0ae6a7af882bbdd7d5ae7e99ecfd0e244118259b
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-26 14:04:54 +0100

    Write functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,21 +6,42 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.turnStart = functions.https.onRequest((request, response) => {
-  const user = getUser(request);
   console.log(request);
+
+  const user = getUser(request);
+  removePreviousPlayersFromQueue(user);
+  setTopOfQueuePlaying(user);
+
   response.send('turn started');
 });
 
 exports.move = functions.https.onRequest((request, response) => {
+  const user = getUser(request);
+  setGameState(request);
+  updateScore(user, request);
+  fillQueue();
+
   console.log(request);
   response.send('moved');
 });
 
-exports.gameOver = functions.https.onRequest((request, response) => {
+exports.refreshQueue = functions.https.onRequest((request, response) => {
+  fillQueue();
+
   console.log(request);
-  response.send('game over');
+  response.send('filled');
 });
 
 function getUser(request: Request) {
   // TODO
 }
+
+function fillQueue() {}
+
+function setGameState(request: Request) {}
+
+function updateScore(user: any, request: Request) {}
+
+function removePreviousPlayersFromQueue(user) {}
+
+function setTopOfQueuePlaying(user) {}
